2ea0bffebd6953fad89436bef2342403b5b347a4
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-06-06 15:28:05 -0700

    Created About.js and very basic structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroContainer from "./components/HeroContainer";
+import About from "./components/About";
 import Box from "@mui/material/Box";
 
 function App() {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
       <Navbar />
       <HeroContainer />
+      <About />
     </Box>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
+const About = () => {
+  return (
+    <Box>
+      <Typography>Hello, I'm Kat</Typography>
+      <Typography>
+        I’m a full stack web developer living in Los Angeles. I love learning
+        and having fun while doing it. Can’t stop tinkering with things that
+        catch my attention. Eager to contribute to the creation of meaningful
+        tools and share what I can make with others. Love music, from La Fania
+        to Joy Division. Drop by any time!
+      </Typography>
+      <Box>a list of links to my socials</Box>
+      <Box>a button to... download my resume???</Box>
+    </Box>
+  );
+};
+
+export default About;
