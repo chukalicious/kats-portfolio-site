98cc285213d221db05b0247b0ce9317c8cd17a2d
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-06-06 15:01:49 -0700

    Added Card component to HeroContainer, which holds an avatar and text for the Hero area

--- src/components/HeroContainer.js
+++ src/components/HeroContainer.js
@@ -1,15 +1,58 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import ImageListItem from "@mui/material/ImageListItem";
+import Paper from "@mui/material/Paper";
+import heroImg from "../assets/heroImg.jpeg";
+import headshot from "../assets/headshot.png";
 
 const HeroContainer = () => {
   return (
-    <Box sx={{ display: "flex" }}>
-      <Typography
-        sx={{ border: "red 1px solid", color: "red", marginTop: "4rem" }}
+    <Box
+      style={{
+        bgimage: `url(${heroImg}')`,
+        // backgroundRepeat: "no-repeat",
+        // backgroundAttachment: "fixed",
+        // backgroundPosition: "center",
+      }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        // border: "red 2px solid",
+        minHeight: "50vh",
+        marginTop: "4rem",
+        // backgroundImage: `url(${heroImg}')`,
+        // backgroundRepeat: "no-repeat",
+        // backgroundAttachment: "fixed",
+        // backgroundPosition: "center",
+      }}
+    >
+      <Card
+        sx={{
+          display: "flex",
+          border: "red 1px solid",
+          flexDirection: "column",
+        }}
       >
-        {" "}
-        This is the Hero Container
-      </Typography>
+        <Avatar
+          sx={{
+            height: "auto",
+            width: "35%",
+            objectFit: "cover",
+            margin: "1rem auto",
+          }}
+        >
+          <CardMedia component="img" image={headshot}></CardMedia>
+        </Avatar>
+        <CardContent sx={{ margin: "0 auto", textAlign: "center" }}>
+          <Typography>Hi, I'm Kat Alicea</Typography>
+          <Typography>Full Stack Web Developer</Typography>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
