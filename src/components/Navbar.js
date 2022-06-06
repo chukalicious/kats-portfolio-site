2968f5caebb651a1aa2ee8f9871ac161892abf70
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-06-06 11:30:50 -0700

    Attempted the methods recommended by the Material UI documentation regarding fixing the position of the AppBar to no avail. The following component keeps on getting hidden behind the AppBar component. For now, the fix will be to add a margin to the HeroComponent while a fix is found.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,28 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { Fragment } from "react";
 
 import NavMenu from "./NavMenu";
 
 const Navbar = () => {
   return (
-    <AppBar position="sticky" sx={{ justifyContent: "center" }}>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Typography>Katiuska Alicea Web Developer</Typography>
-        <NavMenu />
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar
+        // position="sticky"
+        sx={{
+          justifyContent: "center",
+          //   position: "sticky",
+          maxHeight: "auto",
+        }}
+      >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography>Katiuska Alicea Web Developer</Typography>
+          <NavMenu />
+        </Toolbar>
+      </AppBar>
+      {/* <Toolbar /> */}
+    </>
   );
 };
 
