5c439c48af3444d76e52bcf83a6054d6ac04c610
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-06-06 17:44:13 -0700

    made small change to test the continuous deployment

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ import Typography from "@mui/material/Typography";
 const About = () => {
   return (
     <Box>
-      <Typography>Hello, I'm Kat</Typography>
+      <Typography variant="h1">Hello, I'm Kat</Typography>
       <Typography>
         I’m a full stack web developer living in Los Angeles. I love learning
         and having fun while doing it. Can’t stop tinkering with things that
         catch my attention. Eager to contribute to the creation of meaningful
         tools and share what I can make with others. Love music, from La Fania
-        to Joy Division. Drop by any time!
+        All Stars to Joy Division. Drop by any time!
       </Typography>
       <Box>a list of links to my socials</Box>
       <Box>a button to... download my resume???</Box>
